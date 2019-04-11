d4887ff482a0120ec1d59da2046ec6f088d729db
Joshua Z. Zhang <cheungchih@gmail.com>
2019-04-11 16:02:34 -0700

    initial benchmarking codes

--- tests/benchmark/bench_opencv.py
+++ tests/benchmark/bench_opencv.py
@@ -4,11 +4,13 @@ import cv2
 
 cap = cv2.VideoCapture('test2.mp4')
 tic = time.time()
+cnt = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
     if not ret:
         break
-print('elapsed time for sequential read: ', time.time() - tic)
+    cnt += 1
+print(cnt, ' frames. Elapsed time for sequential read: ', time.time() - tic)
 
 cap.release()
 cv2.destroyAllWindows()
--- tests/cpp/video/test_ffmpeg_video_reader.cc
+++ tests/cpp/video/test_ffmpeg_video_reader.cc
@@ -1,23 +1,28 @@
 #include <decord/video_interface.h>
 #include <dmlc/logging.h>
+#include <chrono>
 // #include <dmlc/io.h>
 // #include <gtest/gtest.h>
 
 using NDArray = decord::runtime::NDArray;
 int main(int argc, const char **argv) {
-    auto vr = decord::GetVideoReader("test.mp4");
+    auto vr = decord::GetVideoReader("test2.mp4");
     LOG(INFO) << "Frame count: " << vr->GetFrameCount();
     vr->QueryStreams();
     NDArray array;
     int cnt = 0;
+    auto start = std::chrono::system_clock::now();
     while (1) {
         array = vr->NextFrame();
         // LOG(INFO) << array.Size();
         if (!array.Size()) break;
         cnt++;
-        if (cnt > 200) break;
+        // if (cnt > 200) break;
         // LOG(INFO) << "Frame: " << cnt;
     }
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = end - start;
+    LOG(INFO) << cnt << " frame. Elapsed time: " << elapsed.count() / 1000000.0;
     return 0;
 
     LOG(INFO) << " reset by seek";
