96bcc8c73d0430ce32567109b5f8d506980e597d
Joshua Z. Zhang <cheungchih@gmail.com>
2019-05-17 17:52:35 -0700

    init samplers

---
+++ src/sampler/sampler_interface.h
@@ -0,0 +1,33 @@
+/*!
+ *  Copyright (c) 2019 by Contributors if not otherwise specified
+ * \file sampler_interface.h
+ * \brief Sampler interface
+ */
+
+#ifndef DECORD_SAMPLER_SAMPLER_INTERFACE_H_
+#define DECORD_SAMPLER_SAMPLER_INTERFACE_H_
+
+#include <memory>
+#include <vector>
+#include <utility>
+
+namespace decord {
+namespace sampler {
+
+using SamplerIndex = std::pair<size_t, int64_t>;
+using Samples = std::vector<SamplerIndex>;
+
+class SamplerInterface {
+    public:
+        virtual ~SamplerInterface() = default;
+        virtual void Reset() = 0;
+        virtual bool HasNext() const = 0;
+        virtual Samples Next() = 0;
+        virtual int64_t Size() const = 0;
+};  // class SamplerInterface
+
+using SamplerPtr = std::unique_ptr<SamplerInterface>;
+    
+}  // sampler
+}  // decord
+#endif
