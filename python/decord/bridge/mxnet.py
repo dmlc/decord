6020002dc94c66d6dcf3d0d1e2600b5280ad55f5
Joshua Z. Zhang <cheungchih@gmail.com>
2020-05-01 16:58:24 -0700

    add tvm bridge, add tests for bridges

--- python/decord/bridge/mxnet.py
+++ python/decord/bridge/mxnet.py
@@ -15,11 +15,11 @@ def try_import_mxnet():
         CPU: `pip install mxnet-mkl`, GPU: `pip install mxnet-cu100mkl`"
     return try_import('mxnet', msg)
 
-def from_decord(decord_arr):
+def to_mxnet(decord_arr):
     """from decord to mxnet, no copy"""
     mx = try_import_mxnet()
     return mx.nd.from_dlpack(decord_arr.to_dlpack())
 
-def to_decord(mxnet_arr):
+def from_mxnet(mxnet_arr):
     """from mxnet to decord, no copy"""
     return _from_dlpack(mxnet_arr.to_dlpack_for_read())
---
+++ tests/python/unittests/test_bridges.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import decord
+from decord import VideoReader
+from decord.bridge import *
+
+def _get_default_test_video():
+    return VideoReader(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'examples', 'flipping_a_pancake.mkv')))
+
+def test_mxnet_bridge():
+    try:
+        from decord.bridge.mxnet import try_import_mxnet
+        mxnet = try_import_mxnet()
+        vr = _get_default_test_video()
+        with use_mxnet():
+            frame = vr[0]
+            assert isinstance(frame, mxnet.nd.NDArray)
+            native_frame = bridge_in(frame)
+        assert isinstance(native_frame, decord.nd.NDArray), type(native_frame)
+    except ImportError:
+        print('Skip test mxnet bridge as mxnet is not found')
+
+def test_torch_bridge():
+    try:
+        from decord.bridge.torchdl import try_import_torch
+        import torch
+        vr = _get_default_test_video()
+        with use_torch():
+            frame = vr[0]
+            assert isinstance(frame, torch.Tensor), type(frame)
+            native_frame = bridge_in(frame)
+        assert isinstance(native_frame, decord.nd.NDArray), type(native_frame)
+    except ImportError:
+        print('Skip test torchdl bridge as torch is not found')
+
+def test_tf_bridge():
+    try:
+        from decord.bridge.tf import try_import_tfdl
+        import tensorflow as tf
+        vr = _get_default_test_video()
+        with use_tensorflow():
+            frame = vr[0]
+            assert isinstance(frame, tf.Tensor), type(frame)
+            native_frame = bridge_in(frame)
+        assert isinstance(native_frame, decord.nd.NDArray), type(native_frame)
+    except ImportError:
+        print('Skip test tensorflow bridge as tf is not found')
+
+def test_tvm_bridge():
+    try:
+        from decord.bridge.tvm import try_import_tvm
+        tvm = try_import_tvm()
+        vr = _get_default_test_video()
+        with use_tvm():
+            frame = vr[0]
+            assert isinstance(frame, tvm.nd.NDArray)
+            native_frame = bridge_in(frame)
+        assert isinstance(native_frame, decord.nd.NDArray), type(native_frame)
+    except ImportError:
+        print('Skip test tvm bridge as tvm is not found')
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule()
