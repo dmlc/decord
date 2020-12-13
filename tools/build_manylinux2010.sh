c6631777371443278e6ee1e2ec67a4e511e71343
Joshua Zhang <cheungchih@gmail.com>
2020-12-12 23:57:48 -0800

    fix build

--- tools/build_manylinux2010.sh
+++ tools/build_manylinux2010.sh
@@ -3,24 +3,42 @@
 set -e
 
 # build tools
-yum install -y autoconf automake gcc gcc-c++ git libtool make nasm pkgconfig wget opencv zlib-devel dbus-devel lua-devel zvbi libdvdread-devel  libdc1394-devel libxcb-devel xcb-util-devel libxml2-devel mesa-libGLU-devel pulseaudio-libs-devel alsa-lib-devel libgcrypt-devel qt-devel
-yum --enablerepo=epel install -y yasm libva-devel libass-devel libkate-devel libbluray-devel libdvdnav-devel libcddb-devel libmodplug-devel
+yum install -y autoconf automake bzip2 bzip2-devel freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel
 
 # cmake
 cd ~
 curl -O -L https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh
 chmod +x ./cmake-3.19.1-Linux-x86_64.sh
 ./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr/local/bin
-cmake -version
+/usr/local/bin/cmake -version
 
 # workspace
 mkdir ~/ffmpeg_sources
+
+# nasm
+cd ~/ffmpeg_sources
+curl -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2
+tar xjf nasm-2.14.02.tar.bz2
+cd nasm-2.14.02
+./autogen.sh
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+
+# yasm
+cd ~/ffmpeg_sources
+curl -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
 
 # libx264
 cd ~/ffmpeg_sources
 git clone --depth 1 https://code.videolan.org/videolan/x264.git
 cd x264
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-shared --enable-pic
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-shared --enable-pic
 make
 make install
 
@@ -28,7 +46,7 @@ make install
 cd ~/ffmpeg_sources
 git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
 cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm --enable-shared --enable-pic
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm --enable-shared --enable-pic
 make
 make install
 
@@ -62,6 +80,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR/..
 mkdir build
 cd build
-cmake .. -DUSE_CUDA=0
+/usr/local/bin/cmake .. -DUSE_CUDA=0
 make -j$(nproc)
 popd
