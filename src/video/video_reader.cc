6abd15c1b049257c9cdc3aeff18cdfd04df9514c
johnnynunez <johnnynuca14@gmail.com>
2025-06-13 02:55:57 +0200

    ffmpeg7

--- src/video/video_reader.cc
+++ src/video/video_reader.cc
@@ -146,7 +146,7 @@ VideoReader::~VideoReader(){
 void VideoReader::SetVideoStream(int stream_nb) {
     if (!fmt_ctx_) return;
         const AVCodec *dec;
-    int st_nb = av_find_best_stream(fmt_ctx_.get(), AVMEDIA_TYPE_VIDEO, stream_nb, -1, &dec, 0);
+    int st_nb = av_find_best_stream(fmt_ctx_.get(), AVMEDIA_TYPE_VIDEO, stream_nb, -1, (const AVCodec**)&dec, 0);
     // LOG(INFO) << "find best stream: " << st_nb;
     CHECK_GE(st_nb, 0) << "ERROR cannot find video stream with wanted index: " << stream_nb;
     // initialize the mem for codec context
